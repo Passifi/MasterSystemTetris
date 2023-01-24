3f18134122c35111d441d3717d94bc90b631ac40
Pascal Wigger <pascal.wigger@gmx.de>
2023-01-24 21:26:54 +0100

    Complete overhaul

--- AsmFiles/MasterSystem/Tetris/tetris3.asm
+++ AsmFiles/MasterSystem/Tetris/tetris3.asm
@@ -1,10 +1,12 @@
 XPosStart equ 12
 YPosStart equ 5
 BlockSprite equ 4
+Channel1FullVolume equ %10010000 
 
 StartOffPlayArea equ $16
 LEVEL0 equ 2
 ControlTimer equ 10
+PSG equ &7f
 
 	include "smsbasic.z80.asm"
 	
@@ -81,8 +83,7 @@ init:
 	ld (SwapBuffer),hl 
 	ld ix,(SwapBuffer)
 	
-	 
-	call setPiece ; what does this do ? 
+	; what does this do ? 
 
 	ei
 
@@ -95,372 +96,33 @@ main:
 int: ; reminder this is the interrupt sequence so it gets executed on the interrupt 
 	di 
 	in a,(&bf)
-	; reset input bool
-	ld a,0 
-	ld (b_registeredInput),a 
-	; ______________________________________________________
-	; controlBlock
-	;_______________________________________________________
-	;check whether button is pressed
-	in a,(ControlRg) ; load in current state of controlRg
-	xor a,255 ; inverts the bits because 0 equals pressed ! 
-	cp 0 
-
-	jp z, TimingFun ; if a 0 goto TimingFun
-	ld b,a 
-	ld a,(InputBuffer) ; load InputBuffer in a
-	and &F0 ;blank lower bits 
-	or b ; add b to input buffer 
-	ld (InputBuffer),a 
-
-;************************************************************
-;The following issues with the TimingFun block and tetris is
-; that there are different timers needed for 
-; rendering new states
-; updating falling state
-; applying controlChanges 
-; it needs to be possible for a genuine tetris feeling to move the piece left, right and rotate it while it is NOT falling ! 
-; simple fix for this would be to only apply the higher level timer (comparison ticks+1 with TimeInterval ) for the actual falling 
-; and processing the logic for controller inputs and rerendering everytime the lowerbit of a rolls over 
-
-TimingFun: 	; set flags for the actual game code
-	push af 
-		ld a,0 
-		ld (Timerflags_Music_Control_Logic),a 
-		; check whether to execute control block 
-		ld a, ControlTimer
-		ld d, a ; set d to TimeInterval 
-		; check whether ticks has filled up
-		ld a, (ticks)
-		inc a 
-		ld (ticks),a 
-		or a 
-		cp d 
-		jp nz, LogicTest
-			
-			ld a, (Timerflags_Music_Control_Logic)
-			or 2 
-			ld (Timerflags_Music_Control_Logic),a 
-
-LogicTest:
-		ld a,(TimeInterval)
-		ld d,a 
-		ld a,(ticks)
-		or a 
-		cp 40
-		jp nz, endOfTimingBlock
-			ld a,0
-			ld (ticks),a
-			ld a,(ticks+1)
-			inc a 
-			ld (ticks+1),a
-			or a 
-			cp d 
-			jp nz, endOfTimingBlock
-
-				ld a,(Timerflags_Music_Control_Logic)
-				or 4 
-				ld (Timerflags_Music_Control_Logic),a 
-				ld a,0
-				ld (ticks+1),a
-
-endOfTimingBlock:
-	pop af 
-; end of timing block
-
-;branching
-	ld a,(Timerflags_Music_Control_Logic)
-	ld b,a 
-	and 2 
-	cp 2
-	call z, controlPiece
-	ld a,(Timerflags_Music_Control_Logic)
-	or a 
-	and 4 
-	cp 4
-	call z, logicBlock 
+	ld a,(ticks) ; make a macro for this 
 	
-	ei 
-	ret 
-
-logicBlock:
-
-	ld a,(PieceYPos)
 	inc a 
-	ld (PieceYPos),a
-	cp 20 ; did the piece hit the floor 
-	; here we need a check whether it hit other pieces
-	jp c, afterCollisionCheck ; if a is smaller than 20 we don't reset a
-	ld a,3
-	ld (PieceYPos),a
-	call initTMap ; complete wipe of the tmpa
-	;call updatePieces
-    ;call pickPiece
-	call setPiece
-afterCollisionCheck:
-	
-	
-	ret 
-
-controlPiece:
-	
-	; problem sometimes registers two conescutive rights or left 
-	; check Right 
-	
-	ld a,(InputBuffer)
-	and a,8
-	jp z, checkLeft 
-	srl a 
-	srl a
-	srl a 
-	ld b,a 
-	ld a,(PieceXPos)
+	ld (ticks),a 
 	or a 
-	adc b 
-	or a 
-	cp &11
-	jp c, rightBoundOkay
-	ld a,&11
-rightBoundOkay:
-	ld (PieceXPos),a 
-	ld a,0
-	ld (InputBuffer),a 
-	call initTMap ; complete wipe of the tmpa
-	;call updatePieces
-    ;call pickPiece
-	call setPiece
-	ret 
-checkLeft:
-	ld a,(InputBuffer)
-	and a,4 
-	srl a 
-	srl a 
-	ld b,a 
-	ld a,(PieceXPos)
-	or a 
-	sub b
-	or a 
-	cp &0A
-	jp nc, leftBoundOkay
-	ld a,&0A
-leftBoundOkay:
-	ld (PieceXPos),a 
-	ld a,0 
-	ld (InputBuffer), a
-	call initTMap ; complete wipe of the tmpa
-	;call updatePieces
-    ;call pickPiece
-	call setPiece
-	ret  
-
-pickPiece:
-	; randomly picks a piece and loads the pointer address into IX
-	ld a,(RNGIndex)
-	ld c,a
-	ld b,0 
-	or a ;clear carry flag ?
-	ld hl,RNGStart
-	adc hl,bc 
-	inc a 
-	ld (RNGIndex),a ; increment and save new RNGIndex
-	
-	ld a,(hl) ; get piece no from rng list
-	ld (PieceNo),a 
-	ld hl,PiecePointers
 	cp 0
-	jp z,endLoopInc
-inchl:
-	inc hl 
-	dec a 
-	cp 0
-	jp nz, inchl
-endLoopInc:
-	ld b,(hl)
-	or a 
-	adc b
-	ld ixl,a 
-	inc hl
-	ld a,(hl)
-	ld ixh,a
+	jp z, interuptReturn
+	ld a, Channel1FullVolume; turn channel 1 on 
+	out (PSG),a
+	ld a,%10001101
+	out (PSG),a 
+	ld a,%00110101sssssssssssssssssssssss 
+	out (PSG),a 
 
-setPiece: ; setPieceDef: loads in the currently saved x,y position and modifies them using the current PiecePointer data, so that the correct piece is rendered
-; then converts the tileposition data, to values that make sense to the vdp and sends the data out
 
-	ld a,(ix+1); load Block Y Position 
-	ld b,a  
-    ld a,(PieceYPos)
-	or a ; clear carry?? 
-	adc b 
-	push af 
-		ld a,(ix+0); load Block X Position 
-		ld b,a  
-		ld a,(PieceXPos) ; convert XPos to byte position 
-		or a 
-		adc b 
-		rl a ; why the roation ? 
-		ld (TmpX),a 
-	pop af 
-	ld b,0 
-	or a ; set carry to 0
-	sla a ; multiply by 2 
-	rl b  ; rotate carry into b in case number is bigger then 255
-	sla a ; 
-	rl b 
-	sla a ; 
-	rl b 
-	sla a ; 
-	rl b 
-	sla a ; 
-	rl b 
-	sla a ; 
-	rl b 
-	ld c,a 
-	; now bc is the number in a multplied by 64 because on line has 64 tiles
-	ld hl, TileMap
-	or a 
+
+interuptReturn: 
+	ei
+	ret 
 	
-	ld a,(TmpX) ; add x to the overall position
-	or a 
-	adc c 
-	ld c,a 
-	adc hl,bc ; add position value to tilemap start
-	; now hl is position of the block in the tilemap 
-	call prepareVDP
-	ld a,4 ; sprite number
-	out (vdpData),a 
 
-	ld a,(ix+3); load Block Y Position 
-	ld b,a  
-    ld a,(PieceYPos)
-	or a
-	adc b 
 	
-	push af 
-		ld a,(ix+2); load Block X Position 
-		ld b,a  
-		ld a,(PieceXPos) ; convert XPos to byte position 
-		or a 
-		adc b 
-		rl a 
-		ld (TmpX),a 
-	pop af 
-	
-	ld b,0 
-	or a 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	ld c,a 
 
-	ld hl, TileMap
-	or a 
-	
-	ld a,(TmpX)
-	or a 
-	adc c 
-	ld c,a
-	adc hl,bc ; add position value to tilemap start 
-	call prepareVDP
-	ld a,4
-	out (vdpData),a 
 
-	ld a,(ix+5); load Block Y Position 
-	ld b,a  
-    ld a,(PieceYPos)
-	or a
-	adc b 
-	push af 
-		ld a,(ix+4); load Block X Position 
-		ld b,a  
-		ld a,(PieceXPos) ; convert XPos to byte position 
-		or a 
-		adc b 
-		rl a 
-		ld (TmpX),a 
-	pop af 
-	ld b,0 
-	or a 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	ld c,a 
 
-	ld hl, TileMap
-	or a 
-	
-	ld a,(TmpX)
-	or a 
-	adc c 
-	ld c,a
-	adc hl,bc ; add position value to tilemap start 
-	call prepareVDP
-	ld a,4 
-	out (vdpData),a 
 
-	ld a,(ix+7); load Block Y Position 
-	ld b,a  
-    ld a,(PieceYPos)
-	or a
-	adc b 
-	push af 
-		ld a,(ix+6); load Block X Position 
-		ld b,a  
-		ld a,(PieceXPos) ; convert XPos to byte position 
-		or a 
-		adc b 
-		rl a 
-		ld (TmpX),a 
-		
-	pop af 
-	ld b,0 
-	or a 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	sla a ; shifts left bottom bit 0 Don't forget it :)
-	rl b 
-	ld c,a 
 
-	ld hl, TileMap
-	or a 
-	
-	ld a,(TmpX)
-	or a 
-	adc c 
-	ld c,a
-	adc hl,bc ; add position value to tilemap start 
-	call prepareVDP
-	ld a,4
-	out (vdpData),a 
-
-	ret
-	 
-	
 PiecePointers:
 	dw Square, Line, Triangle, LRight, LLeft,ZigZag1,ZigZag2
 	
